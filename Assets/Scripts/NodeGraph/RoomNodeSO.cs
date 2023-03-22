a4c71829de48552f3a3bd1b17fbf99c75a57d0c3
lopeselio <lopeselio@gmail.com>
2023-03-22 15:17:10 +0530

    added Scriptable Objects in the Node Graph Editor

---
+++ Assets/Scripts/NodeGraph/RoomNodeSO.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomNodeSO : ScriptableObject
+{
+    // Start is called before the first frame update
+    [HideInInspector] public string id;
+    [HideInInspector] public List<string> parentRoomNodeIDList = new List<string>();
+    [HideInInspector] public List<string> childRoomNodeIDList = new List<string>();
+    [HideInInspector] public RoomNodeGraphSO roomNodeGraph;
+    public RoomNodeTypeSO roomNodeType;
+    [HideInInspector] public RoomNodeTypeListSO roomNodeTypeList;
+
+
+
+
+}
---
+++ Assets/Scripts/NodeGraph/RoomNodeTypeSO.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RoomNodeType_", menuName = "Scriptable Objects/Dungeon/Room Node Type")]
+public class RoomNodeTypeSO : ScriptableObject
+{
+    public string roomNodeTypeName;
+
+    #region Header
+    [Header("Only flag the RoomNodeTypes that should be visible in the editor")]
+    #endregion Header
+    public bool displayInNodeGraphEditor = true;
+    #region Header
+    [Header("One Type Should be A Corridor")]
+    #endregion Header
+    public bool isCorridor;
+    #region Header
+    [Header("One Type Should be A CorridorNS")]
+    #endregion Header
+    public bool isCorridorNS;
+    #region Header
+    [Header("One Type Should be A CorridorEW")]
+    #endregion Header
+    public bool isCorridorEW;
+    #region Header
+    [Header("One Type Should be An Entrance")]
+    #endregion Header
+    public bool isEntranceW;
+    #region Header
+    [Header("One Type Should be A Boss Room")]
+    #endregion Header
+    public bool isBossRoom;
+    #region Header
+    [Header("One Type Should be None (Unassigned")]
+    #endregion Header
+    public bool isNone;
+}
